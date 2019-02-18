48abb4c5c7ae77a965489f27c40d76365aee1f50
Superthing1 <dohagan150799@live.co.uk>
2019-02-18 15:18:11 +0000

    .OBJ file loading possible, need to add Normals support in Mesh Class

--- src/com/company/engine/Utils.java
+++ src/com/company/engine/Utils.java
@@ -1,6 +1,9 @@
 package com.company.engine;
 
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -13,6 +16,22 @@ public class Utils {
             result = scanner.useDelimiter("\\A").next();
         }
         return result;
+    }
+
+    public static List<String> readAllLines(String fileName) throws Exception {
+        List<String> lines = new ArrayList<>();
+
+        try(BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(Class.forName(Utils.class.getName()).getResourceAsStream(fileName))
+        )) {
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                lines.add(line);
+            }
+        }
+
+        return lines;
     }
 
     public static int[] listToIntArray(List<Integer> list) {
